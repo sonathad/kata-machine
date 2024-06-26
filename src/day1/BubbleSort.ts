96fa1a278c13b96ee48f436c412e6e4a3cdb0ed1
Thanos Dimitriades <thanos@sonath.dev>
2024-06-26 22:00:00 +0300

    feat: bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,8 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arr.length - i; j++) {
+            if (arr[j - 1] > arr[j])
+                [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+        }
+    }
 }
