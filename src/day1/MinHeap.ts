7cd9daad60b093031840a9332cdafd3754c03f20
Thanos Dimitriades <thanos@sonath.dev>
2024-06-27 23:21:46 +0300

    feat: add heap

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,87 @@
 export default class MinHeap {
     public length: number;
-
-    
+    private data: number[];
 
     constructor() {
+        this.data = [];
+        this.length = 0;
     }
 
     insert(value: number): void {
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
+    }
 
-}
     delete(): number {
+        if (this.length === 0) {
+            return -1;
+        }
 
-}
+        const out = this.data[0];
+        this.length--;
+        if (this.length === 0) {
+            this.data = [];
+            return out;
+        }
+
+        this.data[0] = this.data[this.length];
+        this.heapifyDown(0);
+        return out;
+    }
+
+    private parent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
+
+    private leftChild(idx: number): number {
+        return idx * 2 + 1;
+    }
+    private rightChild(idx: number): number {
+        return idx * 2 + 2;
+    }
+
+    private heapifyUp(idx: number): void {
+        if (idx === 0) {
+            return;
+        }
+
+        const p = this.parent(idx);
+        const parentValue = this.data[p];
+        const value = this.data[idx];
+
+        if (parentValue > value) {
+            // go up!
+            this.data[idx] = parentValue;
+            this.data[p] = value;
+            this.heapifyUp(p);
+        }
+    }
+
+    private heapifyDown(idx: number): void {
+        if (idx >= this.length) {
+            return;
+        }
+
+        const lIdx = this.leftChild(idx);
+        const rIdx = this.rightChild(idx);
+
+        if (rIdx >= this.length || lIdx >= this.length) {
+            return;
+        }
+
+        const lValue = this.data[lIdx];
+        const rValue = this.data[rIdx];
+        const value = this.data[idx];
+
+        if (lValue > rValue && value > rValue) {
+            this.data[idx] = rValue;
+            this.data[rIdx] = value;
+            this.heapifyDown(rIdx);
+        } else if (rValue > lValue && value > lValue) {
+            this.data[idx] = lValue;
+            this.data[lIdx] = value;
+            this.heapifyDown(lIdx);
+        }
+    }
 }
