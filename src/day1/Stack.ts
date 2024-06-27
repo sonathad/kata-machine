7d58fd3e54689045423a52f3d4ab69a19f9b52e2
Thanos Dimitriades <thanos@sonath.dev>
2024-06-27 10:49:21 +0300

    feat: add stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,41 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const curr: Node<T> = { value: item };
+        this.length++;
+        if (!this.head) {
+            this.head = curr;
+            return;
+        }
 
-}
+        curr.next = this.head;
+        this.head = curr;
+    }
+
     pop(): T | undefined {
+        if (!this.head) return;
+        this.length--;
 
-}
+        const curr = this.head;
+        this.head = this.head.next;
+        curr.next = undefined;
+
+        return curr.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
