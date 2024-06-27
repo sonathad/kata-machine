51097173058460744d4157605fe8ee5b31f0a856
Thanos Dimitriades <thanos@sonath.dev>
2024-06-27 19:11:03 +0300

    feat: breadth-first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,19 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+export default function bfs(head: BinaryNode<number> | null, needle: number): boolean {
+    const q: Array<BinaryNode<number> | null> = [head];
 
+    while (q.length) {
+        const next = q.shift() as BinaryNode<number>;
+        if (next.value === needle) {
+            return true;
+        }
+        if (next.left) {
+            q.push(next.left)
+        }
+
+        if (next.right) {
+            q.push(next.right);
+        }
+    }
+
+    return false;
 }
