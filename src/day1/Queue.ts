2e536fada35230a811f878a8639cf7b0d31ff1d9
Thanos Dimitriades <thanos@sonath.dev>
2024-06-27 10:17:45 +0300

    feat: Queue data structure

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const curr: Node<T> = { value: item };
+        this.length++;
 
-}
+        if (!this.tail) {
+            this.tail = this.head = curr;
+            return;
+        }
+
+        this.tail.next = curr;
+        this.tail = curr;
+    }
+
     deque(): T | undefined {
+        if (!this.head) return;
 
-}
+        this.length--;
+
+        const curr = this.head;
+        this.head = this.head.next;
+
+        if (!this.head) {
+            this.tail = undefined;
+        }
+
+        return curr.value;
+
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
